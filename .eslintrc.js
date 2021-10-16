cdc1fe0e0173ff23e5d361def796348f73580820
Anh Dau <redbeanboy98@gmail.com>
2021-10-16 15:08:31 +0700

    Configure ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  rules: {
+    // Place to specify ESLint rules
+    // Overwrites rules specified from the extended configs
+    'semi': 'off',
+    '@typescript-eslint/semi': 'error',
+    'no-extra-semi': 'off',
+    '@typescript-eslint/no-extra-semi': 'error',
+    'comma-dangle': 'off',
+    '@typescript-eslint/comma-dangle': 'error',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'eol-last': 'error',
+    'quotes': [
+      'error',
+      'single',
+      {
+        'allowTemplateLiterals': true
+      }
+    ],
+    'indent': ['error', 2]
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  }
+};
