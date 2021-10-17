69b507d5b5344339bd0c3dee3fc9bd370f16b661
Anh Dau <redbeanboy98@gmail.com>
2021-10-17 14:37:48 +0700

    Restructure api module for scalability

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,47 +1,23 @@
-import axios, { AxiosResponse } from 'axios';
+import {
+  fetchCategories,
+  createCategory,
+  getCategory,
+  updateCategory,
+  deleteCategory
+} from './categories';
 
-import { Category, RawCategory } from 'models';
+// export const BASE_URL = 'https://whats-the-dish-api.herokuapp.com';
+export const BASE_URL = 'http://127.0.0.1:5000/api/v1';
 
-interface CommonApiResponse {
+export interface CommonApiResponse {
   included?: any;
   [extraProps: string]: any;
 }
 
-type FetchCategoriesApiResponse =
-  AxiosResponse<CommonApiResponse & { data: Category[] }>;
-
-type CreateCategoryApiResponse =
-  AxiosResponse<CommonApiResponse & { data: Category }>;
-
-type GetCategoryApiResponse =
-  AxiosResponse<CommonApiResponse & { data: Category }>;
-
-type UpdateCategoryApiResponse =
-  AxiosResponse<CommonApiResponse & { data: Category }>;
-
-type DeleteCategoryApiResponse = AxiosResponse<CommonApiResponse>;
-
-
-// const BASE_URL = 'https://whats-the-dish-api.herokuapp.com';
-const BASE_URL = 'http://127.0.0.1:5000/api/v1';
-
-/* Categories REST APIs */
-
-export const fetchCategories = (): Promise<FetchCategoriesApiResponse> =>
-  axios.get(`${BASE_URL}/categories`);
-
-export const createCategory =
-  (category: RawCategory): Promise<CreateCategoryApiResponse> =>
-    axios.post(`${BASE_URL}/categories`, category);
-
-export const getCategory =
-  (id: string): Promise<GetCategoryApiResponse> =>
-    axios.get(`${BASE_URL}/categories/${id}`);
-
-export const updateCategory =
-  (id: string, category: RawCategory): Promise<UpdateCategoryApiResponse> =>
-    axios.patch(`${BASE_URL}/categories/${id}`, category);
-
-export const deleteCategory =
-  (id: string): Promise<DeleteCategoryApiResponse> =>
-    axios.delete(`${BASE_URL}/categories/${id}`);
+export {
+  fetchCategories,
+  createCategory,
+  getCategory,
+  updateCategory,
+  deleteCategory
+};
