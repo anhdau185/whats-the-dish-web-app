1857eb34f51542b2c00d4131b66a351fc1bebf9a
Anh Dau <redbeanboy98@gmail.com>
2021-10-20 22:04:51 +0700

    Add util function for getting category images

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,23 @@
+import isEmpty from 'lodash/fp/isEmpty';
+
+import { Category } from 'models';
+
 export interface EmptyProps {
   [props: string]: never;
 }
+
+interface CategoryImages {
+  categoryImage?: string;
+  categoryAlbum: string[];
+}
+
+export const getCategoryImages = (category: Category): CategoryImages => {
+  const allImages = category.attributes.images;
+
+  if (isEmpty(allImages)) return { categoryAlbum: [] };
+
+  return {
+    categoryImage: allImages[0],
+    categoryAlbum: allImages.slice(1)
+  };
+};
