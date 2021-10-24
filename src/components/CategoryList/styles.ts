c885d87f37d407a0ff5a2e5faec0fb4dc629732a
Anh Dau <anh.dau@employmenthero.com>
2021-10-24 23:23:31 +0700

    Change what to render when list is empty

--- src/components/CategoryList/styles.ts
+++ src/components/CategoryList/styles.ts
@@ -1,6 +1,9 @@
 import { makeStyles, Theme } from '@material-ui/core/styles';
 
 export default makeStyles((theme: Theme) => ({
+  emptyCategories: {
+    color: '#333'
+  },
   mainContainer: {
     display: 'flex',
     alignItems: 'flex-start'
