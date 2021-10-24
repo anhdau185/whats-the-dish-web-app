cc8dcf0cc0c888718edb5f2f4712fd33ce7f71a8
Anh Dau <anh.dau@employmenthero.com>
2021-10-24 10:20:27 +0700

    Add dish model

---
+++ src/models/dish.ts
@@ -0,0 +1,26 @@
+interface RawDishAttributes {
+  name: string;
+  title: string;
+  description: string | null;
+  images: string[];
+  places: string[];
+}
+
+interface DishAttributes extends RawDishAttributes {
+  likeCount: number;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export interface RawDish {
+  attributes: RawDishAttributes;
+}
+
+export interface Dish {
+  id: string;
+  type?: 'dishes';
+  attributes: DishAttributes;
+  relationships?: Record<string, unknown>;
+}
+
+export type NullableDish = Dish | null;
