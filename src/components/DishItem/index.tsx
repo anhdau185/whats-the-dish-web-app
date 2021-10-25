5f0a23f75aa3d99c3ba09c4a8e46a3ddc412a8c4
Anh Dau <redbeanboy98@gmail.com>
2021-10-25 13:20:42 +0700

    Add DishItem component for showing single dish item (wip)

--- src/components/DishItem/index.tsx
+++ src/components/DishItem/index.tsx
@@ -1,9 +1,78 @@
-import React, { FC } from 'react';
+import React, { FC, useMemo } from 'react';
+import { useDispatch } from 'react-redux';
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Typography
+} from '@material-ui/core';
 
 import { Dish } from 'models';
+import { getDishImages } from 'utils';
+import setCurrentDish from 'actions/setCurrentDish';
+import removeCurrentDish from 'actions/removeCurrentDish';
+import MoreMenu, { MoreMenuItems } from 'components/MoreMenu';
+
+import useStyles from './styles';
+
+const DEFAULT_IMAGE_URL =
+  'https://dl.dropboxusercontent.com/s/0krcni2sgpktto9/no-img.jpg';
 
 const DishItem: FC<{ dish: Dish }> = ({ dish }) => {
-  return null;
+  const classes = useStyles();
+  const dispatch = useDispatch();
+
+  const dishImage = useMemo(
+    () => getDishImages(dish).dishImage || DEFAULT_IMAGE_URL,
+    [dish]
+  );
+
+  const menuItems = useMemo<MoreMenuItems>(
+    () => ({
+      'Edit': () => dispatch(setCurrentDish(dish)),
+      'Delete': () => {
+        if (confirm('Delete this dish?')) {
+          // dispatch(
+          //   deleteDish({
+          //     id: dish.id,
+          //     onCompletion: () => dispatch(removeCurrentDish())
+          //   })
+          // );
+        }
+      }
+    }),
+    [dish]
+  );
+
+  return (
+    <Card className={classes.card}>
+      <img
+        src={dishImage}
+        title={dish.attributes.title}
+        alt={dish.attributes.title}
+      />
+      <div className={classes.overlay2}>
+        <MoreMenu items={menuItems} color="white" />
+      </div>
+      <CardContent>
+        <Typography variant="h5" gutterBottom>
+          {dish.attributes.title}
+        </Typography>
+        <Typography variant="body2" color="textSecondary">
+          {dish.attributes.description}
+        </Typography>
+      </CardContent>
+      <CardActions className={classes.cardActions}>
+        <Button
+          size="small"
+          color="primary"
+        >
+          View details
+        </Button>
+      </CardActions>
+    </Card>
+  );
 };
 
 export default DishItem;
