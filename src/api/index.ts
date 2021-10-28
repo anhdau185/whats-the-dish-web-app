ce9189cb476b2c3e157156e43622c655b0a78007
Anh Dau <anh.dau@employmenthero.com>
2021-10-28 08:42:46 +0700

    Add dishes api consumption

--- src/api/index.ts
+++ src/api/index.ts
@@ -6,8 +6,16 @@ import {
   deleteCategory
 } from './categories';
 
-// export const BASE_URL = 'https://whats-the-dish-api.herokuapp.com';
-export const BASE_URL = 'http://127.0.0.1:5000/api/v1';
+import {
+  fetchDishes,
+  createDish,
+  getDish,
+  updateDish,
+  deleteDish
+} from './dishes';
+
+// export const BASE_URL = 'https://whats-the-dish-api.herokuapp.com'; // production host
+export const BASE_URL = 'http://127.0.0.1:5000/api/v1';  // local host
 
 export interface CommonApiResponse {
   included?: unknown;
@@ -19,5 +27,10 @@ export {
   createCategory,
   getCategory,
   updateCategory,
-  deleteCategory
+  deleteCategory,
+  fetchDishes,
+  createDish,
+  getDish,
+  updateDish,
+  deleteDish
 };
