87fbe2bb92c8493e5165b4ef663ddd6cc3caed3f
Anh Dau <anh.dau@employmenthero.com>
2021-10-28 08:57:41 +0700

    Refactor hooks module

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,42 +1,7 @@
-import { useCallback, useState } from 'react';
+import useGetCategoryApi from './useGetCategoryApi';
+import useGetDishApi from './useGetDishApi';
 
-import { NullableCategory, Dish } from 'models';
-import * as api from 'api';
-import {
-  GetCategoryApiOptions,
-  SingleCategoryApiResponse
-} from 'api/categories';
-
-interface GetCategoryHookResult {
-  data: NullableCategory;
-  includedData: Dish[];
-  fetchData: () => void;
-  loading: boolean;
-  error: any;
-}
-
-export const useGetCategoryApi =
-  (id: string, params?: GetCategoryApiOptions): GetCategoryHookResult => {
-    const [response, setResponse] = useState<SingleCategoryApiResponse | null>(null);
-    const [loading, setLoading] = useState<boolean>(false);
-    const [error, setError] = useState<any>(null);
-
-    const data = response?.data || null;
-    const includedData = response?.included || [];
-
-    const fetchData = useCallback(() => {
-      setLoading(true);
-      api.getCategory(id, params)
-        .then(({ data: apiResponse }) => setResponse(apiResponse))
-        .catch(setError)
-        .finally(() => setLoading(false));
-    }, [id, params]);
-
-    return {
-      data,
-      includedData,
-      fetchData,
-      loading,
-      error
-    };
-  };
+export {
+  useGetCategoryApi,
+  useGetDishApi
+};
