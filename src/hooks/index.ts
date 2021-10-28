b2400ac4c020815c0aa77b824c05d6d49fb5ca26
Anh Dau <redbeanboy98@gmail.com>
2021-10-28 11:04:45 +0700

    Add custom hook for calling fetch dishes api

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,7 +1,9 @@
 import useGetCategoryApi from './useGetCategoryApi';
 import useGetDishApi from './useGetDishApi';
+import useFetchDishesApi from './useFetchDishesApi';
 
 export {
   useGetCategoryApi,
-  useGetDishApi
+  useGetDishApi,
+  useFetchDishesApi
 };
