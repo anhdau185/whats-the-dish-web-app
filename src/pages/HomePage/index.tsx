9ce8fd8b50c49c2d2894b5baf6a46b8e663e579d
Anh Dau <redbeanboy98@gmail.com>
2022-01-01 11:40:39 +0700

    Rebuild homepage UI

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,22 +1,39 @@
 import React, { FC } from 'react';
-import { Link } from 'react-router-dom';
-import { Container, Grow, Grid } from '@material-ui/core';
+import { Container, Grow, Typography } from '@material-ui/core';
 
 import { EmptyProps } from 'utils';
+import { HomeItem } from 'models';
+import HomeList from 'components/HomeList';
 
-const HomePage: FC<EmptyProps> = () => {
-  return (
-    <Container maxWidth="lg">
-      <Grow in>
-        <Container>
-          <Grid container spacing={4}>
-            <Link to="/categories">See all categories</Link>
-            <Link to="/dishes">See all dishes</Link>
-          </Grid>
-        </Container>
-      </Grow>
-    </Container>
-  );
-};
+import { StyledWrapper } from './styles';
+
+const HOME_ITEMS: HomeItem[] = [
+  {
+    url: '/categories',
+    title: 'See all categories',
+    description: 'Find your favorite types of food.'
+  },
+  {
+    url: '/dishes',
+    title: 'See all dishes',
+    description: 'Find your favorite foods or drinks.'
+  }
+];
+
+const HomePage: FC<EmptyProps> = () => (
+  <Container maxWidth="lg">
+    <Grow in>
+      <StyledWrapper>
+        <Typography variant="h3" color="textPrimary">
+          Welcome to <span>What&apos;s The Dish</span>!
+        </Typography>
+        <Typography variant="h6" color="textPrimary">
+          What are your dishes today?<br />Get started by choosing what you might want to see:
+        </Typography>
+        <HomeList items={HOME_ITEMS} />
+      </StyledWrapper>
+    </Grow>
+  </Container>
+);
 
 export default HomePage;
---
+++ src/pages/HomePage/styles.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const StyledWrapper = styled.div`
+  padding: 5rem 0;
+  text-align: center;
+
+  h3 {
+    margin-bottom: 1rem;
+
+    span {
+      color: rgb(126, 197, 254);
+    }
+  }
+
+  h6 {
+    font-size: 1.5rem;
+    line-height: 1.4;
+    margin-bottom: 1.5rem;
+  }
+`;
