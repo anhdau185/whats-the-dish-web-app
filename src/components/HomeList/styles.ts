17aa64edf8b4c100d66c1b8e8aec19203daa9cde
Anh Dau <redbeanboy98@gmail.com>
2022-01-01 11:35:20 +0700

    Build home list and home item components

---
+++ src/components/HomeList/styles.ts
@@ -0,0 +1,56 @@
+import styled from 'styled-components';
+
+export const StyledGrid = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+  max-width: 800px;
+  margin: 0 auto;
+
+  @media (max-width: 600px) {
+    width: 100%;
+    flex-direction: column;
+  }
+`;
+
+export const StyledListItem = styled.div`
+  a {
+    display: block;
+    width: 250px;
+    margin: 1rem;
+    padding: 1.5rem;
+    text-align: left;
+    text-decoration: none;
+    border: 1px solid #eaeaea;
+    border-radius: 10px;
+    transition: border-color 0.15s ease;
+
+    h2 {
+      margin: 0;
+      font-size: 1.5rem;
+      font-weight: 700;
+      transition: color 0.15s ease;
+      font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+        Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+    }
+
+    p {
+      margin-top: 0.5rem;
+      font-size: 1.25rem;
+      line-height: 1.5;
+      transition: color 0.15s ease;
+    }
+
+    &:hover,
+    &:focus,
+    &:active {
+      border-color: #0070f3;
+
+      h2,
+      p {
+        color: #0070f3;
+      }
+    }
+  }
+`;
