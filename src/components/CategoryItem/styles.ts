7d622df7cccad79531f4dca247b5d27388c8df19
Anh Dau <redbeanboy98@gmail.com>
2022-01-02 11:22:33 +0700

    Refine category item UI

--- src/components/CategoryItem/styles.ts
+++ src/components/CategoryItem/styles.ts
@@ -48,6 +48,10 @@ export const StyledCard = styled(Card)`
     border-radius: 15px;
     height: 100%;
     justify-content: space-between;
+    box-shadow:
+      0px 2px 12px -1px rgba(0, 0, 0, 0.2),
+      0px 1px 1px 0px rgba(0, 0, 0, 0.14),
+      0px 1px 3px 0px rgba(0, 0, 0, 0.12);
   }
 `;
 
