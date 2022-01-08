7cbb69c212832fb760f342bb39bc385b6c0a4f5d
Anh Dau <redbeanboy98@gmail.com>
2022-01-08 08:46:47 +0700

    Refactor edit category title & description components

--- src/components/EditableCategoryTitle/index.tsx
+++ src/components/EditableCategoryTitle/index.tsx
@@ -1,36 +1,21 @@
 import React, { FC, useCallback, useRef, useState } from 'react';
-import { Typography, TextField, IconButton } from '@material-ui/core';
+import { Typography } from '@material-ui/core';
 import Edit from '@material-ui/icons/Edit';
 import Check from '@material-ui/icons/Check';
 import Close from '@material-ui/icons/Close';
-import styled from 'styled-components';
 
 import { Category, RawCategory } from 'models';
 import useUpdateCategoryApi from 'hooks/useUpdateCategoryApi';
 
-const TitleWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  min-height: 60px;
-  margin-bottom: 0.5rem;
-`;
-
-const StyledTextField = styled(TextField)`
-  div {
-    font-size: 2.125rem;
-  }
-`;
-
-const StyledIconButton = styled(IconButton)`
-  padding: 8px;
-  height: max-content;
-`;
+import { StyledIconButton, StyledTextField, TitleWrapper } from './styles';
 
 const EditableCategoryTitle: FC<{ category: Category }> = ({ category }) => {
   const originalValue = useRef<string>(category.attributes.title);
   const [displayValue, setDisplayValue] = useState<string>(category.attributes.title);
   const [editMode, setEditMode] = useState<boolean>(false);
-  const { fetchData: updateCategory } = useUpdateCategoryApi();
+  const { fetchData: updateCategory } = useUpdateCategoryApi({
+    onFailure: () => window.alert('Failed to update category title.')
+  });
 
   const enterEditMode = () => setEditMode(true);
 
