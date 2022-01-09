96b00412edd0344dfbdae64b75ab54851137c815
Anh Dau <redbeanboy98@gmail.com>
2022-01-09 23:58:06 +0700

    Fix annoying console warning about Progress component

--- src/components/Progress/index.tsx
+++ src/components/Progress/index.tsx
@@ -2,19 +2,14 @@ import React, { FC } from 'react';
 import { LinearProgress } from '@material-ui/core';
 import styled from 'styled-components';
 
-interface ProgressProps { 
-  loading: boolean;
-}
-
-const StyledProgress = styled(LinearProgress)<ProgressProps>`
-  visibility: ${({ loading }) => loading ? 'visible' : 'hidden'};
+const StyledProgress = styled(LinearProgress)`
   width: 100%;
   margin-bottom: 1rem;
 `;
 
-const Progress: FC<ProgressProps> = ({ loading, children }) => (
+const Progress: FC<{ loading: boolean }> = ({ loading, children }) => (
   <>
-    <StyledProgress loading={loading} />
+    <StyledProgress style={{ visibility: loading ? 'visible' : 'hidden' }} />
     {children}
   </>
 );
