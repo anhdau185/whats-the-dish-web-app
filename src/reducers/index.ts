ee2be87fee24d4003fa672b0d93d5fe2df6b93f5
Anh Dau <redbeanboy98@gmail.com>
2022-01-10 12:49:23 +0700

    Remove current category reducer

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,11 +1,10 @@
 import { Reducer, CombinedState, combineReducers } from 'redux';
 
-import { Category, NullableCategory, NullableDish } from 'models';
+import { Category, NullableDish } from 'models';
 import { AppAction } from 'actions';
 
 import testDataReducer from './testDataReducer';
 import categoriesReducer from './categoriesReducer';
-import currentCategoryReducer from './currentCategoryReducer';
 import currentDishReducer from './currentDishReducer';
 import appLoadingReducer from './appLoadingReducer';
 import fetchCategoriesApiCallReducer, {
@@ -15,7 +14,6 @@ import fetchCategoriesApiCallReducer, {
 export interface GlobalState {
   testData: number;
   categories: Category[];
-  currentCategory: NullableCategory;
   currentDish: NullableDish;
   appLoading: boolean;
   fetchCategoriesApiCall: FetchCategoriesApiCall;
@@ -27,7 +25,6 @@ type RootReducer =
 const rootReducer: RootReducer = combineReducers({
   testData: testDataReducer,
   categories: categoriesReducer,
-  currentCategory: currentCategoryReducer,
   currentDish: currentDishReducer,
   appLoading: appLoadingReducer,
   fetchCategoriesApiCall: fetchCategoriesApiCallReducer
--- src/reducers/state/index.ts
+++ src/reducers/state/index.ts
@@ -1,11 +1,8 @@
 import { GlobalState } from 'reducers';
-import { Category, NullableCategory, NullableDish } from 'models';
+import { Category, NullableDish } from 'models';
 
 export const categoriesSelector =
   (state: Readonly<GlobalState>): Category[] => state.categories;
-
-export const currentCategorySelector =
-  (state: Readonly<GlobalState>): NullableCategory => state.currentCategory;
 
 export const currentDishSelector =
   (state: Readonly<GlobalState>): NullableDish => state.currentDish;
