989cf3b6de422989dad32a3fc6a905a715cb85db
Anh Dau <redbeanboy98@gmail.com>
2022-01-10 12:58:32 +0700

    Remove update category action & AC

--- src/reducers/categoriesReducer.ts
+++ src/reducers/categoriesReducer.ts
@@ -1,25 +1,15 @@
 import { Reducer } from 'redux';
 
 import { Category } from 'models';
-import {
-  UpdateCategoryAction
-} from 'actions';
-import {
-  UPDATE_CATEGORY
-} from 'actions/types';
 
 type CategoriesReducer = Reducer<
   Category[],
-  UpdateCategoryAction
+  any
 >;
 
 const categoriesReducer: CategoriesReducer =
   (prevState = [], action) => {
     switch (action.type) {
-      case UPDATE_CATEGORY:
-        return prevState.map(
-          item => item.id === action.payload.id ? action.payload : item
-        );
       default:
         return prevState;
     }
