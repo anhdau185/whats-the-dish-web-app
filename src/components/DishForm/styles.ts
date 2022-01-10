f521d584f86237888c630ef89b428c47724002b9
Anh Dau <anh.dau@employmenthero.com>
2022-01-10 21:57:18 +0700

    Migrate DishForm to styled-components

--- src/components/DishForm/styles.ts
+++ src/components/DishForm/styles.ts
@@ -1,23 +1,25 @@
-import { makeStyles, Theme } from '@material-ui/core/styles';
+import styled from 'styled-components';
+import { Paper, Typography } from '@material-ui/core';
 
-export default makeStyles((theme: Theme) => ({
-  root: {
-    '& .MuiTextField-root': {
-      margin: theme.spacing(1)
-    }
-  },
-  paper: {
-    padding: theme.spacing(2),
-    borderRadius: 12,
-    boxShadow: `
-      0px 2px 12px -1px rgba(0, 0, 0, 0.2),
-      0px 1px 3px 0px rgba(0, 0, 0, 0.14),
-      0px 1px 5px 0px rgba(0, 0, 0, 0.12);
-    `
-  },
-  form: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'center'
+export const CustomPaper = styled(Paper)`
+  padding: 1rem;
+  border-radius: 12px;
+  box-shadow: 0px 2px 12px -1px rgba(0, 0, 0, 0.2),
+    0px 1px 3px 0px rgba(0, 0, 0, 0.14),
+    0px 1px 5px 0px rgba(0, 0, 0, 0.12);
+
+  .MuiTextField-root {
+    margin: 8px 0;
   }
-}));
+`;
+
+export const StyledForm = styled.form`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
+
+export const FormHeading = styled(Typography)`
+  width: 100%;
+  margin-bottom: 0.25rem;
+`;
