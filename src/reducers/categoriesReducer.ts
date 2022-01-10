bf470e82263855945d7b3471ddf1f5ff3c54fcb6
Anh Dau <anh.dau@employmenthero.com>
2022-01-10 12:28:12 +0700

    Remove unused create category action & AC

--- src/reducers/categoriesReducer.ts
+++ src/reducers/categoriesReducer.ts
@@ -3,20 +3,18 @@ import { Reducer } from 'redux';
 import { Category } from 'models';
 import {
   FetchCategoriesAction,
-  CreateCategoryAction,
   UpdateCategoryAction,
   DeleteCategoryAction
 } from 'actions';
 import {
   FETCH_ALL_CATEGORIES,
-  CREATE_CATEGORY,
   UPDATE_CATEGORY,
   DELETE_CATEGORY
 } from 'actions/types';
 
 type CategoriesReducer = Reducer<
   Category[],
-  FetchCategoriesAction | CreateCategoryAction | UpdateCategoryAction | DeleteCategoryAction
+  FetchCategoriesAction | UpdateCategoryAction | DeleteCategoryAction
 >;
 
 const categoriesReducer: CategoriesReducer =
@@ -24,8 +22,6 @@ const categoriesReducer: CategoriesReducer =
     switch (action.type) {
       case FETCH_ALL_CATEGORIES:
         return action.payload;
-      case CREATE_CATEGORY:
-        return [...prevState, action.payload];
       case UPDATE_CATEGORY:
         return prevState.map(
           item => item.id === action.payload.id ? action.payload : item
