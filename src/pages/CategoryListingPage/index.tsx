1a1f7cf21e2c11b79ba14feb7832967f48bfe824
Anh Dau <anh.dau@employmenthero.com>
2022-01-10 19:09:46 +0700

    Rework api error handling

--- src/pages/CategoryListingPage/index.tsx
+++ src/pages/CategoryListingPage/index.tsx
@@ -1,49 +1,24 @@
 import React, { FC, useEffect } from 'react';
 import { Container, Grow, Grid, Typography } from '@material-ui/core';
 
-import { Category } from 'models';
 import { EmptyProps } from 'utils';
 import { useFetchCategoriesApi } from 'hooks';
 import Progress from 'components/Progress';
 import CategoryList from 'components/CategoryList';
 import CategoryForm from 'components/CategoryForm';
 
-interface ListContentProps {
-  data: Category[];
-  error: any;
-  loading: boolean;
-}
-
-const ListContent: FC<ListContentProps> = ({ data, error, loading }) => {
-  if (loading) return (
-    <Typography
-      variant="h5"
-      color="textSecondary"
-      style={{ marginBottom: '0.5em' }}
-    >
-      We&apos;re getting the categories...
-    </Typography>
-  );
-
-  if (error != null) return (
-    <Typography
-      variant="h5"
-      color="textSecondary"
-      style={{ marginBottom: '0.5em' }}
-    >
-      An error occurred while fetching the categories
-      {error?.message ? ` (${error?.message})` : ''}.
-    </Typography>
-  );
-
-  return <CategoryList categories={data} />;
-};
-
 const CategoryListingPage: FC<EmptyProps> = () => {
-  const { data, error, loading, fetchData } = useFetchCategoriesApi();
+  const {
+    data: categories,
+    error,
+    loading: isFetchingCategories,
+    fetchData: fetchCategories
+  } = useFetchCategoriesApi();
+
+  const errorOccurred = error != null;
 
   useEffect(() => {
-    fetchData({
+    fetchCategories({
       include_dishes: false,
       order_by: 'title',
       order_direction: 'asc'
@@ -52,7 +27,7 @@ const CategoryListingPage: FC<EmptyProps> = () => {
 
   return (
     <Container maxWidth="lg">
-      <Progress loading={loading}>
+      <Progress loading={isFetchingCategories}>
         <Grow in>
           <Container>
             <Grid
@@ -62,7 +37,21 @@ const CategoryListingPage: FC<EmptyProps> = () => {
               spacing={3}
             >
               <Grid item xs={12} sm={7}>
-                <ListContent data={data} error={error} loading={loading} />
+                {errorOccurred ? (
+                  <Typography
+                    variant="h5"
+                    color="textSecondary"
+                    style={{ marginBottom: '0.5em' }}
+                  >
+                    An error occurred while fetching the categories
+                    {error?.message ? ` (${error?.message})` : ''}.
+                  </Typography>
+                ) : (
+                  <CategoryList
+                    categories={categories}
+                    emptyText={isFetchingCategories ? `We're getting the categories...` : undefined}
+                  />
+                )}
               </Grid>
               <Grid item xs={12} sm={4}>
                 <CategoryForm />
