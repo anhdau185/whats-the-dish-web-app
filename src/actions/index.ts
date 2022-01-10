7418486f9904b46e0feca4f72e485b09071da1d6
Anh Dau <anh.dau@employmenthero.com>
2022-01-10 13:08:14 +0700

    Remove testData reducer, action & AC

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,22 +1,16 @@
-import { IncrementTestDataAction } from './incrementTestData';
-import { DecrementTestDataAction } from './decrementTestData';
 import { SetCurrentDishAction } from './setCurrentDish';
 import { RemoveCurrentDishAction } from './removeCurrentDish';
 import { EnableAppLoadingAction } from './enableAppLoading';
 import { DisableAppLoadingAction } from './disableAppLoading';
 import { ApiCallFetchCategoriesAction } from './fetchCategoriesAC';
 
-type AppAction = IncrementTestDataAction
-  | DecrementTestDataAction
-  | SetCurrentDishAction
+type AppAction = SetCurrentDishAction
   | RemoveCurrentDishAction
   | EnableAppLoadingAction
   | DisableAppLoadingAction
   | ApiCallFetchCategoriesAction;
 
 export type {
-  IncrementTestDataAction,
-  DecrementTestDataAction,
   SetCurrentDishAction,
   RemoveCurrentDishAction,
   EnableAppLoadingAction,
