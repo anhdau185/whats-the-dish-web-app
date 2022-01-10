3faf5096b93381cccd13068e4f879194b863b728
Anh Dau <anh.dau@employmenthero.com>
2022-01-10 13:25:57 +0700

    Remove enable & disable app loading actions & ACs

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,22 +1,16 @@
 import { SetCurrentDishAction } from './setCurrentDish';
 import { RemoveCurrentDishAction } from './removeCurrentDish';
-import { EnableAppLoadingAction } from './enableAppLoading';
-import { DisableAppLoadingAction } from './disableAppLoading';
 import { SetAppLoadingAction } from './setAppLoadingAC';
 import { ApiCallFetchCategoriesAction } from './fetchCategoriesAC';
 
 type AppAction = SetCurrentDishAction
   | RemoveCurrentDishAction
-  | EnableAppLoadingAction
-  | DisableAppLoadingAction
   | SetAppLoadingAction
   | ApiCallFetchCategoriesAction;
 
 export type {
   SetCurrentDishAction,
   RemoveCurrentDishAction,
-  EnableAppLoadingAction,
-  DisableAppLoadingAction,
   SetAppLoadingAction,
   ApiCallFetchCategoriesAction,
   AppAction
--- src/reducers/appLoadingReducer.ts
+++ src/reducers/appLoadingReducer.ts
@@ -1,26 +1,13 @@
 import { Reducer } from 'redux';
 
-import {
-  EnableAppLoadingAction,
-  DisableAppLoadingAction,
-  SetAppLoadingAction
-} from 'actions';
-import {
-  ENABLE_APP_LOADING,
-  DISABLE_APP_LOADING,
-  SET_APP_LOADING
-} from 'actions/types';
+import { SetAppLoadingAction } from 'actions';
+import { SET_APP_LOADING } from 'actions/types';
 
-type AppLoadingReducer =
-  Reducer<boolean, EnableAppLoadingAction | DisableAppLoadingAction | SetAppLoadingAction>;
+type AppLoadingReducer = Reducer<boolean, SetAppLoadingAction>;
 
 const appLoadingReducer: AppLoadingReducer =
   (prevState = false, action) => {
     switch (action.type) {
-      case ENABLE_APP_LOADING:
-        return true;
-      case DISABLE_APP_LOADING:
-        return false;
       case SET_APP_LOADING:
         return action.payload;
       default:
