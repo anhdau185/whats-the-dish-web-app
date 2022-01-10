3a1e19651eb5c903c96f7283b382e48e09b105c7
Anh Dau <anh.dau@employmenthero.com>
2022-01-10 13:21:22 +0700

    Add set app loading action & AC

--- src/reducers/appLoadingReducer.ts
+++ src/reducers/appLoadingReducer.ts
@@ -2,15 +2,17 @@ import { Reducer } from 'redux';
 
 import {
   EnableAppLoadingAction,
-  DisableAppLoadingAction
+  DisableAppLoadingAction,
+  SetAppLoadingAction
 } from 'actions';
 import {
   ENABLE_APP_LOADING,
-  DISABLE_APP_LOADING
+  DISABLE_APP_LOADING,
+  SET_APP_LOADING
 } from 'actions/types';
 
 type AppLoadingReducer =
-  Reducer<boolean, EnableAppLoadingAction | DisableAppLoadingAction>;
+  Reducer<boolean, EnableAppLoadingAction | DisableAppLoadingAction | SetAppLoadingAction>;
 
 const appLoadingReducer: AppLoadingReducer =
   (prevState = false, action) => {
@@ -19,6 +21,8 @@ const appLoadingReducer: AppLoadingReducer =
         return true;
       case DISABLE_APP_LOADING:
         return false;
+      case SET_APP_LOADING:
+        return action.payload;
       default:
         return prevState;
     }
