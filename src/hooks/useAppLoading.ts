7793deae278da6d0093209210749a805e2def07a
Anh Dau <anh.dau@employmenthero.com>
2022-01-15 07:20:30 +0700

    Add hook to make it easier to use appLoading state

---
+++ src/hooks/useAppLoading.ts
@@ -0,0 +1,23 @@
+import { useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { appLoadingSelector } from 'reducers/state';
+import setAppLoadingAC from 'actions/setAppLoadingAC';
+
+interface AppLoadingHookResult {
+  loading: boolean;
+  setLoading: (value: boolean) => void;
+}
+
+const useAppLoading = (): AppLoadingHookResult => {
+  const dispatch = useDispatch();
+  const loading = useSelector(appLoadingSelector);
+
+  const setLoading = useCallback((value: boolean) => {
+    dispatch(setAppLoadingAC(value));
+  }, []);
+
+  return { loading, setLoading };
+};
+
+export default useAppLoading;
