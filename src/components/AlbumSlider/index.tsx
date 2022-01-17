205d48f0f0d0c2a3ff9da398afd46d9a7d63172c
Anh Dau <redbeanboy98@gmail.com>
2022-01-17 17:55:58 +0700

    AlbumSlider: Add autoplay and empty state

--- src/components/AlbumSlider/index.tsx
+++ src/components/AlbumSlider/index.tsx
@@ -1,7 +1,10 @@
 import React, { FC } from 'react';
-import { Navigation, Pagination } from 'swiper';
+import { Navigation, Pagination, Autoplay } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react/swiper-react';
 import styled from 'styled-components';
+import isEmpty from 'lodash/fp/isEmpty';
+
+import { DEFAULT_IMAGE_URL } from './constants';
 
 import 'swiper/swiper.min.css';
 import 'swiper/modules/navigation/navigation.min.css';
@@ -18,14 +21,20 @@ const AlbumImage = styled.img`
 const AlbumSlider: FC<{ album: string[] }> = ({ album }) => {
   return (
     <Swiper
-      modules={[Navigation, Pagination]}
+      modules={[Navigation, Pagination, Autoplay]}
       spaceBetween={25}
       slidesPerView={1}
+      autoplay={{ delay: 5000 }}
       navigation
       pagination={{ clickable: true, dynamicBullets: true }}
       scrollbar={{ draggable: true }}
       style={{ borderRadius: 10 }}
     >
+      {isEmpty(album) && (
+        <SwiperSlide style={{ textAlign: 'center' }}>
+          <AlbumImage src={DEFAULT_IMAGE_URL} />
+        </SwiperSlide>
+      )}
       {album.map((item, index) => (
         <SwiperSlide key={`detail-img-${index}`} style={{ textAlign: 'center' }}>
           <AlbumImage src={item} />
