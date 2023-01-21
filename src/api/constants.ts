ce0f98e709265f383a3eb22cad949f7aea2941dc
Anh Dau <redbeanboy98@gmail.com>
2023-01-21 14:20:43 +0700

    update api module to use production host

--- src/api/constants.ts
+++ src/api/constants.ts
@@ -1,5 +1,5 @@
-// heroku production host (deprecated)
-// export const BASE_URL = 'https://whats-the-dish-api.herokuapp.com/api/v1';
+// production host
+export const BASE_URL = 'https://dish.thecodinglad.com/api/v1';
 
 // local host for development
-export const BASE_URL = 'http://localhost:5000/api/v1';
+// export const BASE_URL = 'http://localhost:5000/api/v1';
